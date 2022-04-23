58b52c5ffe14eb025ccc65ecb115bf7404c0ac31
Sydney Stokes <sydney.stokes@bison.howard.edu>
2022-04-23 03:24:43 +0000

    Converting nums to uppercase and Shifting

--- myciper.py
+++ myciper.py
@@ -1,8 +1,23 @@
 import sys
  
+newString = ""
+
 # Set Cipher num equal to input num
-shiftNum = sys.argv[1]
-print(shiftNum)
+shiftNum = int(sys.argv[1])
 
 for line in sys.stdin:
-    print(line)
+    newString = ""
+    shiftString = ""
+    line = line.upper()
+
+    #Discard everything but letters
+    for char in line:
+        if ord(char) > 64 and ord(char) < 91:
+            newString += char
+    print(newString)
+
+    #Shift
+    for char in newString:
+        newASC = ord(char) + shiftNum
+        shiftString += chr(newASC)
+    print(shiftString)
